b07d275c469ffd18ccd5901e3db08678e94c66bf
deinernstjetzt <fabian.vanrissenbeck@gmail.com>
2022-12-13 18:03:12 +0100

    solved the second challenge of day-10

--- day-10/day-10.scm
+++ day-10/day-10.scm
@@ -28,3 +28,31 @@
     (if (eq? cycle at-cycle)
 	(* cycle x)
 	(loop (+ x (car inst)) (cdr inst) (+ cycle 1)))))
+
+(define (in-sprite sprite-center x)
+  (or
+   (= (- sprite-center 1) x)
+   (= sprite-center x)
+   (= (+ sprite-center 1) x)))
+
+(define (exec-draw inst)
+  (let loop ((x 1) (inst inst) (cycle 1) (screen '()))
+    (if (> cycle 240)
+	(list->string (reverse screen))
+	(let* ((col (remainder (- cycle 1) 40))
+	       (on (in-sprite x col))
+	       (chr (if on #\# #\.))
+	       (screen (if (= col 39)
+			   (cons #\newline (cons chr screen))
+			   (cons chr screen))))
+	  (loop (+ x (car inst)) (cdr inst) (+ cycle 1) screen)))))
+
+(let* ((input (load-input "input.txt"))
+       (inst (convert-to-addx input))
+       (sig-at (lambda (at) (signal-strength-at inst at)))
+       (fst (apply + (map sig-at (list 20 60 100 140 180 220))))
+       (snd (exec-draw inst)))
+  (display fst)
+  (newline)
+  (display snd))
+ 
